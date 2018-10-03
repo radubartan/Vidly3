af4ab06b318c1db8771c47be229dc5a03ed0c700
RaduBartan <Radu.Bartan@Gmail.com>
2018-10-03 12:54:24 -0400

    https://github.com/radubartan/Vidly.git

---
+++ Vidly3/Controllers/CustomersController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly3.Models;
+using Vidly3.ViewModels;
+
+
+namespace Vidly3.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        // line below was: 'private' CustomersController() - I changed it to 'public' because I was getting this error: 'No parameterless constructor defined for this object.'
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            // the list of customers
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Smith", Id = 1 },
+                new Customer { Name = "Mary Williams", Id = 2 }
+            };
+
+            var viewModel = new RandomMovieViewModel();
+            viewModel.Movie = movie;
+            viewModel.Customers = customers;
+
+            // part of the code from Vidly2
+            //return View(viewModel);
+
+            var customers2 = _context.Customers.ToList();
+            return View(customers2);
+
+            
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+
+        public ActionResult DisplayCustomer(int id)
+        {
+            if (id == 1)
+            {
+                var customer = new List<Customer>
+                {
+                    new Customer { Name = "John Smith", Id = 1 }
+                };
+
+                var viewModel = new RandomMovieViewModel();
+                viewModel.Customers = customer;
+                return View(viewModel);
+            }
+            else if (id == 2)
+            {
+                var customer = new List<Customer>
+                {
+                    new Customer { Name = "Mary Williams", Id = 2 }
+                };
+
+                var viewModel = new RandomMovieViewModel();
+                viewModel.Customers = customer;
+                return View(viewModel);
+            }
+            else
+            {
+                //Response.StatusCode = 404;
+                throw new HttpException(404, "Not found");
+                return View();
+            }
+        }
+    }
+}
---
+++ Vidly3/Models/MembershipType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly3.Models
+{
+    public class MembershipType
+    {
+        // in Entity Framework - every entity must have a key which will be mapped to the primary key of the corresponding table in the database.
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }            // used 'short' because we don't need any values more than 32,000
+        public byte DurationInMonths { get; set; }      // used 'byte' because the largest number we want to store is 12 for 12 months
+        public byte DiscountRate { get; set; }          // used 'byte' because this number is goint to be a percentage between 0 and 100
+    }
+}
+
+
